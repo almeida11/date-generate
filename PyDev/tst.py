e3e1021a3b23c67993e706e7de0312ddd257a9f6
Mateus Almeida <contato.mat55@gmail.com>
2022-10-02 15:01:48 -0300

    remove tst archive
    removido arquivo de teste.

--- PyDev/tst.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-book = openpyxl.Workbook()
-data_page = book['Sheet']
-
-data_page.append(['FOLHA DE SALÁRIOS', "", "", "", "", '22.089,00',])
-data_page.append(['TRANSM. DE ENERGIA', "", "", "", "", '10.323,00',])
-
-book.save('Planilha.xlsx')
